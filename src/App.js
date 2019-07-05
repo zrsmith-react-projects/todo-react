470d8cbb06e789b562a53ca44ea1e314c0b80b59
Zac Smith <zrs3141592@gmail.com>
2019-07-05 17:41:23 -0600

    create state and drill props

--- src/App.js
+++ src/App.js
@@ -5,11 +5,60 @@ import uuid from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
+  state = {
+    items: [
+      {
+        id: 1,
+        title: "do your homework"
+      },
+      {
+        id: 2,
+        title: "go pick up wawa"
+      }
+    ],
+    id: uuid(),
+    item: "",
+    editItem: false
+  };
+
+  handleChange = event => {
+    console.log("handle change");
+  };
+  handleSubmit = event => {
+    console.log("handle submit");
+  };
+  handleClearList = () => {
+    console.log("clear list");
+  };
+  handleDelete = id => {
+    console.log(`handle edit ${id}`);
+  };
+  handleEdit = id => {
+    console.log(`handle edit ${id}`);
+  };
   render() {
+    console.log(this.state);
+
     return (
-      <div className="App">
-        <TodoInput />
-        <TodoList />
+      <div className="container">
+        <div className="row">
+          <div className="col-10 x-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center">todo input</h3>
+
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.state.handleChange}
+              handleSubmit={this.state.handleSubmit}
+              editItem={this.state.editItem}
+            />
+            <TodoList
+              items={this.state.items}
+              handleClearList={this.state.handleClearList}
+              handleDelete={this.state.handleDelete}
+              handleEdit={this.state.handleEdit}
+            />
+          </div>
+        </div>
       </div>
     );
   }
