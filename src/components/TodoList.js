3f3a3dff3b37c63e752b29c52c8c9d848ca1396d
Zac Smith <zrs3141592@gmail.com>
2019-07-05 22:53:12 -0600

    map items and return TodoItem

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,10 +3,29 @@ import TodoItem from "./TodoItem";
 
 export default class TodoList extends Component {
   render() {
+    const { items, handleClearList, handleDelete, handleEdit } = this.props;
     return (
       <section>
-        <h2>Hello from TodoList</h2>
-        <TodoItem />
+        <ul className="list-group my-5">
+          <h3 className="text-capitalize text-center">todo list</h3>
+          {items.map(item => {
+            return (
+              <TodoItem
+                key={item.id}
+                title={item.title}
+                handleDelete={() => handleDelete(item.id)}
+                handleEdit={() => handleEdit(item.id)}
+              />
+            );
+          })}
+          <button
+            type="text"
+            className="btn btn-danger btn-block text-uppercase mt-5"
+            onClick={handleClearList}
+          >
+            clear list
+          </button>
+        </ul>
       </section>
     );
   }
